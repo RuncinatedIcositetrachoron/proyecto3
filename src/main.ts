ade217f3a1e1663dd20d2c2deb9fed3ba113c945
RuncinatedIcositetrachoron <luquicerru@gmail.com>
2026-09-23 15:55:11 -0300

    bugfix with diametrically opposed portals.

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ const config: Phaser.Types.Core.GameConfig = {
     width: 800,
     height: 600,
 
-    backgroundColor: "#2d2d2d",
+    backgroundColor: "#222034",
 
     dom: {
         createContainer: true
