6a445b5e81d1699441025fd08bf736368a9b8ca8
RuncinatedIcositetrachoron <luquicerru@gmail.com>
2026-09-23 11:15:58 -0300

    movement is good now (buffers work better and also you can hold directional)

--- src/gamescene.ts
+++ src/gamescene.ts
@@ -81,9 +81,13 @@ export class GameScene extends Phaser.Scene {
     private playerMoving = false;
     private inputBuffer: string = "";
     private holdBufferOpen: Boolean = false;
+<<<<<<< HEAD
 
     private playerTween: Phaser.Tweens.Tween | undefined;
     private pushTweens: Phaser.Tweens.Tween[] = [];
+=======
+    private playerTween: Phaser.Tweens.Tween | undefined;
+>>>>>>> 6c8570e (movement is good now (buffers work better and also you can hold directional))
     private playerVertical: Boolean = true;
 
     init(data: { level: number, history: GameState[] }) {
@@ -1391,27 +1395,65 @@ export class GameScene extends Phaser.Scene {
                     if (this.movenumber) this.movenumber = false
                     else this.movenumber = true;
                     this.inputBuffer = "left";
+<<<<<<< HEAD
                     if (player.dir === -1 && this.playerVertical === false && this.pushTweens.length === 0) this.playerTween?.setTimeScale(50);
+=======
+                    if (player.dir === -1 && this.playerVertical === false) this.playerTween?.setTimeScale(50);
+>>>>>>> 6c8570e (movement is good now (buffers work better and also you can hold directional))
                 }
 
                 else if (Phaser.Input.Keyboard.JustDown(this.cursors.right!)) {
                     if (this.movenumber) this.movenumber = false
                     else this.movenumber = true;
                     this.inputBuffer = "right";
+<<<<<<< HEAD
                     if (player.dir === 1 && this.playerVertical === false && this.pushTweens.length === 0) this.playerTween?.setTimeScale(50);
+=======
+                    if (player.dir === 1 && this.playerVertical === false) this.playerTween?.setTimeScale(50);
+>>>>>>> 6c8570e (movement is good now (buffers work better and also you can hold directional))
                 }
 
                 else if (Phaser.Input.Keyboard.JustDown(this.cursors.up!)) {
                     if (this.movenumber) this.movenumber = false
                     else this.movenumber = true;
                     this.inputBuffer ="up";
+<<<<<<< HEAD
                     if (player.dir === -1 && this.playerVertical === true && this.pushTweens.length === 0) this.playerTween?.setTimeScale(50);
+=======
+                    if (player.dir === -1 && this.playerVertical === true) this.playerTween?.setTimeScale(50);
+>>>>>>> 6c8570e (movement is good now (buffers work better and also you can hold directional))
                 }
 
                 else if (Phaser.Input.Keyboard.JustDown(this.cursors.down!)) {
                     if (this.movenumber) this.movenumber = false
                     else this.movenumber = true;
                     this.inputBuffer = "down";
+                    if (player.dir === 1 && this.playerVertical === true) this.playerTween?.setTimeScale(50);
+                }
+                else if (this.holdBufferOpen && this.inputBuffer === "") {
+                    if (this.cursors.left!.isDown){
+                        if (this.movenumber) this.movenumber = false
+                        else this.movenumber = true;
+                        this.inputBuffer = "left";
+                    }
+
+                    else if (this.cursors.right!.isDown) {
+                        if (this.movenumber) this.movenumber = false
+                        else this.movenumber = true;
+                        this.inputBuffer = "right";
+                    }
+
+                    else if (this.cursors.up!.isDown) {
+                        if (this.movenumber) this.movenumber = false
+                        else this.movenumber = true;
+                        this.inputBuffer ="up";
+                    }
+
+                    else if (this.cursors.down!.isDown) {
+                        if (this.movenumber) this.movenumber = false
+                        else this.movenumber = true;
+                        this.inputBuffer = "down";
+                    }
                 }
                 else if (this.holdBufferOpen && this.inputBuffer === "") {
                     if (this.cursors.left!.isDown){
